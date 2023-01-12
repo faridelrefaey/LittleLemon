965e062bd61120eb5b59af707afc7f84f2c7345c
faridelrefaey <faridelrefaey98@gmail.com>
2023-01-12 12:15:19 +0200

    Adding tests

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from restaurant.models import Menu, Booking
+class MenuTest(TestCase):
+    def setUp(self):
+        Menu.objects.create(title="IceCream", price=25.50, inventory=5)
+        
+    def test_get_item(self):
+        item = Menu.objects.get(title="IceCream")
+        self.assertEqual(item.__str__(), "IceCream : 25.50")
 
-# Create your tests here.
+class MenuViewTest(TestCase):
+    def setUp(self):
+        Menu.objects.create(title="IceCream", price=25.50, inventory=5)
+        Menu.objects.create(title="Salad", price=5.50, inventory=3)
+        Menu.objects.create(title="Fried Chicken", price=14.50, inventory=2)
+
+    def test_getall(self):
+        item1 = Menu.objects.get(title="IceCream")
+        item2 = Menu.objects.get(title="Salad")
+        item3 = Menu.objects.get(title="Fried Chicken")
+        self.assertEqual(item1.__str__(), "IceCream : 25.50")
+        self.assertEqual(item2.__str__(), "Salad : 5.50")
+        self.assertEqual(item3.__str__(), "Fried Chicken : 14.50")
+        print(item3)
